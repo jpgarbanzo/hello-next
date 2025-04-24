5cf8563cda3fc961c55a50d5648f3818c07febb9
Jose Pablo Garbanzo <jpgarbanzo@gmail.com>
2025-04-23 19:17:17 -0600

    Agregando primera versión de menu

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,37 @@
 import Image from "next/image";
 
+const opcionesDeMenu = [
+  { 
+    id: 1, texto: 'Code', link: '/', isSelected: false
+  }, { 
+    id: 2, texto: 'Issues', link: '/issues', isSelected: true
+  }, { 
+    id: 3, texto: 'Pull-requests', link: '/pulls', isSelected: false
+  }
+];
+
+const menu = function () {
+  return (
+    <nav>
+      <h1>Menu</h1>
+      {opcionesDeMenu.map((opcion) => (
+        <a
+          key={opcion.id}
+          href={opcion.link}
+          >
+            {opcion.texto}
+          </a>
+      ))}
+    </nav>
+  );
+};
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {menu()}
+
         <Image
           className="dark:invert"
           src="/next.svg"
